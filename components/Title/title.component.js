9081479f6aa7b9e8fabb055b07324aafadbff554
evgnidium <evgnidium@gmail.com>
2021-11-12 00:39:04 +0300

    de

---
+++ components/Title/title.component.js
@@ -0,0 +1,26 @@
+import React, {Component } from 'react';
+import { TextInput , View} from 'react-native';
+import PropTypes from "prop-types";
+import styles from "./TextArea.component.style" 
+
+class TextArea extends Component {
+    state = {
+        text : 'фамилия имя отчество ты'
+    }
+    render() {
+        const {...extraProps} = this.props;
+        return (
+            <TextInput
+            
+            {...extraProps}
+            style={[styles.textArea,extraProps.style]}
+            multiline = {true}
+            onChangeText = {(text) => this.setState({text})}
+            value = {this.state.text}
+            
+            />
+        )
+    }
+}
+
+export default TextArea;
