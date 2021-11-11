5956b88013f71b8139db020a6ff7a065799d8c15
evgnidium <evgnidium@gmail.com>
2021-11-11 22:32:37 +0300

    commti

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import TextArea from './components/TextArea/TextArea.component';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View style = {styles.contentWrapper}>
+      <Text style = {styles.textHeader}>Добро пожаловать</Text>
+      <Text style = {styles.lightbottomText}>
+        Шарага в рб
+      </Text>
+      <Text style = {styles.bottomText}>
+        Пес, добро пожаловать. Место
+        где раскрывают способности
+      </Text>
+      <View styles = {styles.inputbox}>
+
+      <TextArea/>
+
+      </View>
+      </View>
     </View>
   );
 }
@@ -14,8 +29,52 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems : "center",
+    alignContent : "center",
+    alignSelf: "center",
+    backgroundColor: '#FFF',
+  },
+  contentWrapper : { 
+    alignContent : "center",
+    justifyContent : "center",
+    alignSelf : "center"
+  },
+  inputbox : {
+    alignItems: "center",
+    justifyContent : "center",
+    alignContent : "center",
+  },
+  lightbottomText: {
+    fontFamily: "Roboto",
+    paddingTop: 20,
+    fontStyle : "normal",
+    fontWeight : "500",
+    fontSize : 17,
+    lineHeight : 22,
+    paddingLeft: 10,
+    letterSpacing: -0.41,
+    color : "#232740"
+  },
+  textHeader : {
+    fontFamily: "Roboto",
+    fontStyle : "normal",
+    fontWeight : "bold",
+    fontSize : 34,
+    lineHeight : 41,
+    color : "#282828",
+    paddingLeft:20
+  },
+  bottomText : {
+    fontFamily: "Roboto",
+    paddingTop: 20,
+    paddingLeft: 5,
+
+    fontStyle : "normal",
+    fontWeight : "normal",
+    fontSize : 20,
+    lineHeight : 25,
+    letterSpacing: 0.38,
+    color : "#282828"
   },
 });
---
+++ components/TextArea/TextArea.component.style.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create ({
+    textArea: {
+        borderColor : 'gray',
+        borderWidth : 2,
+        alignItems : "center",
+        justifyContent : "center",
+        alignContent : "center",
+    }
+})
